a5b46908f13bb5256c5aaf52ac4a30fed542061f
bhattiasad12 <bhattiasad12@gmail.com>
2021-06-03 19:09:38 +0500

    removed the first table now only showing table from http requests (get) added a modal for edit button

--- wwwroot/js/booklist.js
+++ wwwroot/js/booklist.js
@@ -17,9 +17,9 @@ function loadDataTable() {
             { "data": "isbn", "width": "20%" },
             {
                 "data": "id",
-                "render": function (data) {
+                "render": function (data, type, row) {
                     return `<div class="text-center">
-                        <a href="/BookList/Edit?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                        <a onclick=Edit("/BookList/index?id=${data}") class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
                             Edit
                         </a> 
                         &nbsp;                       
@@ -40,27 +40,21 @@ function loadDataTable() {
 }
 
 function Delete(url) {
-    swal({
-        title: "Are you sure?",
-        text: "Once deleted, you will not be able to recover",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true
-    }).then((willDelete) => {
-        if (willDelete) {
-            $.ajax({
-                type: "DELETE",
-                url: url,
-                success: function (data) {
-                    if (data.success) {
-                        toastr.success(data.message);
-                        dataTable.ajax.reload();
-                    }
-                    else {
-                        toastr.error(data.message);
-                    }
-                }
-            });
-        }
+    $.ajax({
+        type: "DELETE",
+        url: url,
+        success: function (data) {
+            if (data.success) {
+                data.message;
+                dataTable.ajax.reload();
+            }
+            else {
+                data.message;
+            }
+        } 
     });
+}
+
+function Edit(url) {
+    $('#edit').modal('show')
 }
