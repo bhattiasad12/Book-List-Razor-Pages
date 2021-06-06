ec8b56a38f061a444b25683ec2e3b12f62482aab
Asad Bhatti <82872024+bhattiasad12@users.noreply.github.com>
2021-06-07 04:36:09 +0500

    changes bootstrap version from 4 to 5 which was causing issues

--- wwwroot/js/booklist.js
+++ wwwroot/js/booklist.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     loadDataTable();
     var data = 0;
 });
+var url = 0;
 
 function loadDataTable() {
     dataTable = $('#DT_load').DataTable({
@@ -21,16 +22,15 @@ function loadDataTable() {
                 "data": "id",
                 "render": function (data) {
                     return `<div class="text-center">
-                        <button onclick=Edit("${data}") class='btn btn-primary form-control text-white' style='cursor:pointer; width:70px;'>
+                        <button onclick=Edit("${data}") class='btn btn-primary' style='cursor:pointer; width:70px;'>
                             Edit
                         </button> 
                         &nbsp;                       
                         &nbsp;
-                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
-                            data-toggle="modal" data-target="#confirmDelete"
-                            data-title="Delete Book" data-message="Are you sure you want to delete this book?")>
+                        <button class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
+                            data-bs-toggle="modal" data-bs-target="#confirmDelete">
                             Delete
-                        </a>
+                        </button>
                         </div>`;
                 }, "width": "40%"
             }
@@ -42,22 +42,38 @@ function loadDataTable() {
     });
 }
 
-function Delete(Id) {
+function Delete(url) {
     $.ajax({
         type: "DELETE",
-        data: {
-            id: Id
-        },
+        url: url,
         success: function (data) {
             if (data.success) {
+                toastr.success(data.message);
                 dataTable.ajax.reload();
             }
             else {
-                alert(data.message);
+                toastr.error(data.message);
             }
         }
-    });
+    })
 }
+
+function Delete(Id) {
+    $.ajax({
+        url: "Book/Delete",
+        type: "DELETE",
+        data: {
+            id: Id
+        }
+    })
+        .done(function (response) {
+            $("#Updateid").val(response.id);
+            $("#name").val(response.name);
+            $("#author").val(response.author);
+            $("#isbn").val(response.isbn);
+        });
+}
+
 function Edit(Id) {
     $.ajax({
         url: "Book/Edit",
@@ -89,23 +105,20 @@ function Update() {
         },
     })
         .done(function (response) {
-            toastr.success(data.message);
+            savedChangesUpdate();
             dataTable.ajax.reload();
         });
 }
 
-$('#confirmDelete').on('show.bs.modal', function (e) {
-    $message = $(e.relatedTarget).attr('data-message');
-    $(this).find('.modal-body p').text($message);
-    $title = $(e.relatedTarget).attr('data-title');
-    $(this).find('.modal-title').text($title);
+var optionToast = {
+    animation: true,
+    delay: 2000
+}
 
-    var form = $(e.relatedTarget).closest('form');
-    $(this).find('.modal-footer #confirm').data('form', form);
-});
-
-$('#confirmDelete').find('.modal-footer #confirm').on('click', function () {
-    dataTable.ajax.reload();
+function savedChangesUpdate() {
+    var toastHTMLElement = document.getElementById("liveToast");
+    var toastElement = new bootstrap.Toast(toastHTMLElement, optionToast);
+    toastElement.show();
+}
 
 
-s});
